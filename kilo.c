6e317fdc663227054bb81d8d49cf5ec6bbcde24c
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-14 15:26:59 +0700

    feat: disable raw mode at exit
    restore terminal original attributes when program exits.

--- kilo.c
+++ kilo.c
@@ -1,7 +1,10 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios; // saves terminal original's attribute'
 void enableRawMode();
+void disableRawMode();
 
 int main()
 {
@@ -13,12 +16,17 @@ int main()
 
 void enableRawMode() 
 {
-    struct termios raw;
-
-    tcgetattr(STDIN_FILENO, &raw);
-
-    raw.c_lflag &= ~(ECHO);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode); // called when the program exits
+    
+    struct termios raw = orig_termios; // assigned orig_termios to raw to make a copy of it
+    raw.c_lflag &= ~(ECHO); // flipped the bits to be 00000000000000000000000000000000
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 
+}
+
+void disableRawMode() 
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
