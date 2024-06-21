6a1eeb44408fe4302702d444cd0fd2f75ae0884d
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-21 21:21:05 +0700

    feat: ctrl-Q to quit

--- kilo.c
+++ kilo.c
@@ -7,6 +7,10 @@
 #include <unistd.h>
 #include <errno.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios; // saves terminal original's attribute'
@@ -35,7 +39,7 @@ int main()
         {
             printf("%d ('%c')\r\n", c, c);
         }
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
     return 0;
 } 
