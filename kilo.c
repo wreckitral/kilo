ad79354d26da9a7cf90589d30ef22aa352cac7c2
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-22 22:40:19 +0700

    feat: improved window size handler

--- kilo.c
+++ kilo.c
@@ -29,6 +29,7 @@ void disableRawMode();
 void die(const char *s);
 char editorReadKey();
 int getWindowsSize(int *rows, int *cols);
+int getCursorPosition(int *rows, int *cols);
 void editorProcessKeypresses();
 void editorRefreshScreen();
 void editorDrawRows();
@@ -107,9 +108,10 @@ int getWindowsSize(int *rows, int *cols)
 {
     struct winsize ws;
     
-    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) 
     {
-        return -1;
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     }
     else 
     {
@@ -117,6 +119,27 @@ int getWindowsSize(int *rows, int *cols)
         *rows = ws.ws_row;
         return 0;
     }
+}
+
+int getCursorPosition(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1)
+    {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    
+    return 0;
 }
 
 /*** input ***/
@@ -141,7 +164,12 @@ void editorDrawRows()
     int y;
     for (y = 0; y < E.screenrows; y++)
     {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenrows - 1)
+        {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
