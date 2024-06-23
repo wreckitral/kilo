c7ef3b6f8d684f678f665b7adea6d108e2f78092
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-23 22:10:09 +0700

    feat: clear lines one at a time

--- kilo.c
+++ kilo.c
@@ -196,6 +196,7 @@ void editorDrawRows(struct abuf *ab)
     {
         abAppend(ab, "~", 1);
 
+        abAppend(ab, "\x1b[k", 3);
         if (y < E.screenrows - 1)
         {
             abAppend(ab, "\r\n", 2);
@@ -207,12 +208,13 @@ void editorRefreshScreen()
 {
     struct abuf ab = ABUF_INIT;
 
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25l", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
