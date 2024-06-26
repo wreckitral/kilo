6a4366fb22ea41a72d1cf54b4cf66ef923f268c7
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-26 20:47:59 +0700

    feat: handle delete key

--- kilo.c
+++ kilo.c
@@ -22,7 +22,8 @@ enum editorKey
     PAGE_UP,
     PAGE_DOWN,
     HOME_KEY,
-    END_KEY
+    END_KEY,
+    DEL_KEY
 };
 
 /*** data ***/
@@ -147,6 +148,7 @@ int editorReadKey()
                     switch (seq[1]) 
                     {
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
