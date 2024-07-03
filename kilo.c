8fddd03c32a719317320b50179ef934585c6b798
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-07-03 19:19:28 +0700

    feat: a line view

--- kilo.c
+++ kilo.c
@@ -1,5 +1,9 @@
 /*** include ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -69,7 +73,7 @@ void editorProcessKeypresses();
 void editorRefreshScreen();
 void editorDrawRows(struct abuf *ab);
 void abAppend(struct abuf *ab, const char *s, int len);
-void editorOpen();
+void editorOpen(char *filename);
 
 /*** init ***/
 
@@ -82,11 +86,14 @@ void initEditor()
     if (getWindowsSize(&E.screenrows, &E.screencols) == -1) die("getWindowsSize");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     enableRawMode();
     initEditor();
-    editorOpen();
+    if (argc >= 2)
+    {
+        editorOpen(argv[1]);
+    }
 
     while (1)
     {
@@ -237,16 +244,28 @@ int getCursorPosition(int *rows, int *cols)
     return 0;
 }
 
-void editorOpen()
+void editorOpen(char *filename)
 {
-    char *line = "Hello, World!";
-    ssize_t linelen = 13;
+    FILE *fp = fopen(filename, "r");
+    if (!fp) die ("fopen");
 
-    E.row.size = linelen;
-    E.row.chars = malloc(linelen + 1);
-    memcpy(E.row.chars, line, linelen);
-    E.row.chars[linelen] = '\0';
-    E.numrows = 1;
+    char *line = NULL;
+    size_t linecap = 0;
+    ssize_t linelen;
+    linelen = getline(&line, &linecap, fp);
+    if (linelen != -1)
+    {
+        while (linelen > 0 && (line[linelen - 1] == '\n' ||
+            line[linelen - 1] == '\r'))
+            linelen++;
+        E.row.size = linelen;
+        E.row.chars = malloc(linelen + 1);
+        memcpy(E.row.chars, line, linelen);
+        E.row.chars[linelen] = '\0';
+        E.numrows = 1;
+    }
+    free(line);
+    fclose(fp);
 }
 
 /*** append buffer ***/
@@ -347,7 +366,7 @@ void editorDrawRows(struct abuf *ab)
     {
         if (y >= E.numrows)
         {
-            if (y == E.screenrows / 3)
+            if (E.numrows == 0 && y == E.screenrows / 3)
             {
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome),
